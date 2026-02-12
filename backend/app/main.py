a1cf540e61a86e66328a006d5caa53c18eeeb5e8
LEE1JU1MIN <is0703ei@ed.ritsumei.ac.jp>
2026-02-12 19:05:32 +0900

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app.include_router(sessions.router)
 app.include_router(funnel.router)
 
 
-origins = ["http://localhost:5173", "http://127.0.0.1:5173"]
+origins = ["http://localhost:5173", "http://127.0.0.1:5173","https://user-punnel-ahurjdnff-lee1ju1mins-projects.vercel.app"]
 extra_origins = os.getenv("FRONTEND_ORIGINS")
 if extra_origins:
     for origin in extra_origins.split(","):
